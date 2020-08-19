3b797b24090856fec94d706dc31e3f9e1ae5a133
Carlos Fonseca <60260089+ecarlosfonseca@users.noreply.github.com>
2020-08-19 13:15:35 +0100

    Update pukapi_API.py

--- pukapi_API.py
+++ pukapi_API.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import requests
 import json
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
+#  Getting all beers data from the API
 url = 'https://api.punkapi.com/v2/beers'
 
-# Getting all beers
 parameters = {'page': 1, 'per_page': 80}
 response = requests.get(url, params=parameters)
 beers = response.json()
@@ -16,8 +19,11 @@ while response.json():
 
 beers_df = pd.DataFrame(beers).reset_index(drop=True).set_index('id')
 
-# beers_df.to_csv("Pukapi_beers.csv")
-# beers_df = pd.read_csv('Pukapi_beers.csv')
+#beers_df.to_csv("Pukapi_beers.csv")
+#beers_df = pd.read_csv('Pukapi_API/Pukapi_beers.csv')
+
+#  EDA
+beers_df['first_brewed'] = pd.to_datetime(beers_df['first_brewed'], infer_datetime_format=True)
 
 analysis_dict = {'variables': list(beers_df.columns.values),
                  'count': list(beers_df.count().values),
@@ -27,4 +33,14 @@ analysis_dict = {'variables': list(beers_df.columns.values),
 
 analysis = pd.DataFrame(analysis_dict)
 print(analysis)
+
+corr_matrix = beers_df.corr()
+lower_triangle_mask = np.triu(np.ones(corr_matrix.shape)).astype(np.bool)
+cor_mat_lower = corr_matrix.mask(lower_triangle_mask)
+
+sns.heatmap(cor_mat_lower,
+            annot=True,
+            cmap='RdBu_r')
+
+plt.show()
 
